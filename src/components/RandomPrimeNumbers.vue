0c639ee109a470ac3912b9181f4316e903db170b
Besnik Korca <besnik.korca@ubt-uni.net>
2022-12-02 16:32:48 +0100

    Create RandomPrimeNumbers comp and use it in App

---
+++ src/components/RandomPrimeNumbers.vue
@@ -0,0 +1,22 @@
+<template>
+    <ul >
+        <li v-for="prime in primeNumbers" v-bind:key="prime" >
+            {{prime}}
+        </li>
+    </ul>
+</template>
+
+<script>
+import listOfPrimes from 'prime-number/list';
+
+export default {
+    data() {
+        return {
+            primeNumbers: listOfPrimes
+        }
+    }
+}
+</script>
+
+<style>
+</style>
