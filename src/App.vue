ac0eb5532fda0a842160268255a218b150cd683b
Besnik Korca <besnik.korca@ubt-uni.net>
2022-12-02 16:45:21 +0100

    Add home page component and use it in App

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <RandomPrimeNumbers />
+  <HomePage />
 </template>
 
 <script>
 import RandomPrimeNumbers from '@/components/RandomPrimeNumbers.vue'
+import HomePage from './components/HomePage.vue';
 
 export default {
   name: 'App',
   components: {
-    RandomPrimeNumbers
+    RandomPrimeNumbers,
+    HomePage
   }
 }
 </script>
